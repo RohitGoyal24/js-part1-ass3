848af2a43e2a303b177d8f6d4f4df83c51cd8007
RohitGoyal24 <39431001+RohitGoyal24@users.noreply.github.com>
2018-12-16 13:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var tempratureInCelcius=40;
+var tempratureInFahrenheit=104;
+var celciusToFahrenheit=(tempratureInCelcius*9)/5+32;
+var fahrenheitToCelcius=((tempratureInFahrenheit-32)*5)/9;
+console.log("Temprature of "+tempratureInCelcius+" celcius, will be "+celciusToFahrenheit+" fahrenheit");
+console.log("Temprature of "+tempratureInFahrenheit+" fahrenheit, will be "+fahrenheitToCelcius+" celcius");
